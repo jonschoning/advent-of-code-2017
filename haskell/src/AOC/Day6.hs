9118c67e527fc9ce48951f00acd12e3644de70e4
Jon Schoning <jonschoning@gmail.com>
2017-12-06 01:28:12 -0600

    impl day6/part2

--- haskell/src/AOC/Day6.hs
+++ haskell/src/AOC/Day6.hs
@@ -3,49 +3,91 @@
 module AOC.Day6 where
 
 import Data.Maybe
-import Data.List
 import qualified Data.ByteString.Char8 as B8
 
 import qualified Data.Vector.Unboxed as V
-import qualified Data.Vector.Unboxed.Mutable as MV
-import qualified Data.Set as S
-
-import Debug.Trace
+import qualified Data.Map.Strict as M
 
 -- * Part One
 
 {-|
+In this area, there are sixteen memory banks; each memory bank can
+hold any number of blocks. The goal of the reallocation routine is to
+balance the blocks between the memory banks.
+
+The reallocation routine operates in cycles. In each cycle, it finds
+the memory bank with the most blocks (ties won by the lowest-numbered
+memory bank) and redistributes those blocks among the banks. To do
+this, it removes all of the blocks from the selected bank, then moves
+to the next (by index) memory bank and inserts one of the blocks. It
+continues doing this until it runs out of blocks; if it reaches the
+last memory bank, it wraps around to the first one.
+
+The debugger would like to know how many redistributions can be done
+before a blocks-in-banks configuration is produced that has been seen
+before.
+
+Given the initial block counts in your puzzle input, how many
+redistribution cycles must be completed before a configuration is
+produced that has been seen before?
 -}
 
 p1 :: B8.ByteString -> Int
-p1 input = go (0 :: Int) (S.empty) (readLines input)
+p1 input = reallocation const 0 (M.empty) (readLines input)
+
+reallocation
+  :: (Int -> Int -> Int) -- ^ done
+  -> Int -- ^ number of total reallocation cycles
+  -> M.Map (V.Vector Int) Int -- ^ map of seen configurations w/ number of reallocation cycles
+  -> V.Vector Int -- ^ current configuration
+  -> Int -- ^ result
+reallocation finish !n_cycles !seen !mem = do
+  case M.lookup mem seen of
+    Just n_last -> finish n_cycles n_last
+    Nothing ->
+      reallocation
+        finish
+        (succ n_cycles)
+        (M.insert mem n_cycles seen)
+        (balance mem)
+
+balance :: V.Vector Int -> V.Vector Int
+balance initial_mem =
+  let max_pos = V.maxIndex initial_mem
+  in go
+       (pred $ initial_mem V.! max_pos)
+       (next_pos initial_mem max_pos)
+       (initial_mem V.// [(max_pos, 0)])
   where
-    go !n !s !v = do
-      if S.member v s
-        then n
-        else do
-          let v' = balance v
-          go (succ n) (S.insert v s) (v')
-    balance v = do
-      let i = V.maxIndex v
-      let v' = V.unsafeUpd v [(i, 0)]
-      balance' (pred $ v V.! i) (next i v') v'
-    balance' sb _ v | sb < 0 = v
-    balance' sb i v = do
-      let v' = V.unsafeUpd v [(i, succ (v V.! i))]
-      balance' (pred sb) (next i v)  v'
-    next i v = 
-      if succ i >= V.length v then 0 else succ i
-      
+    go blocks _ mem
+      | blocks < 0 = mem
+    go blocks pos mem =
+      go
+        (pred blocks)
+        (next_pos mem pos)
+        (mem V.// [(pos, succ (mem V.! pos))])
+
+next_pos :: V.Vector Int -> Int -> Int
+next_pos mem pos =
+  if succ pos >= V.length mem
+    then 0
+    else succ pos
 
 
 -- * Part Two
 
 {-|
+Out of curiosity, the debugger would also like to know the size of the
+loop: starting from a state that has already been seen, how many block
+redistribution cycles must be performed before that same state is seen
+again?
+
+How many cycles are in the infinite loop that arises from the
+configuration in your puzzle input?
 -}
 
 p2 :: B8.ByteString -> Int
-p2 _ = 0
+p2 input = reallocation (-) 0 (M.empty) (readLines input)
 
 -- * Utils
 
--- haskell/tests/Day6Spec.hs
+++ haskell/tests/Day6Spec.hs
@@ -14,17 +14,17 @@ spec :: Spec
 spec = do
 
     describe "Part1" $ do
-      it "0 2 7 0" $ do
+      it "0 2 7 0 -> 5" $ do
         let input = "0\t2\t7\t0\n"
         D6.p1 input `shouldBe` 5
       it "input/day6.txt -> 3156" $ do
         input <- B8.readFile "input/day6.txt"
         D6.p1 input `shouldBe` 3156
 
-    -- describe "Part2" $ do
-    --   it "" $ do
-    --     let input = "0\n3\n0\n1\n-3"
-    --     D6.p2 input `shouldBe` 1
-    --   it "input/day6.txt -> " $ do
-    --     input <- B8.readFile "input/day6.txt"
-    --     D6.p2 input `shouldBe` 1
+    describe "Part2" $ do
+      it "0 2 7 0 -> 4" $ do
+        let input = "0\t2\t7\t0\n"
+        D6.p2 input `shouldBe` 4
+      it "input/day6.txt -> 1610" $ do
+        input <- B8.readFile "input/day6.txt"
+        D6.p2 input `shouldBe` 1610
