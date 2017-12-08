4f61bc373e9c31a676ea448a4b1e82bc97bb59ab
Jon Schoning <jonschoning@gmail.com>
2017-12-08 15:42:33 -0600

    use bytestring

--- haskell/src/Day8.hs
+++ haskell/src/Day8.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 
 module Day8 where
 
@@ -8,72 +9,42 @@ import Data.List
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Map.Strict as M
 
--- | parse input
+type Reg = B8.ByteString
+data Inst = Inst Reg (Int -> Int -> Int) Int Cond
+data Cond = Cond Reg (Int -> Int -> Bool) Int
+
 readLines :: B8.ByteString -> [Inst]
-readLines = fmap (parseLine . words) . lines . B8.unpack
+readLines = fmap (parseLine . B8.words) . B8.lines
 
--- | parse Instruction
-parseLine :: [String] -> Inst
-parseLine (reg:op:amt:_:cReg:cComp:cAmt:[]) =
-  Inst reg (readOp op) (read amt) (Cond cReg (readComp cComp) (read cAmt))
-parseLine s = error (concat s)
+parseLine :: [B8.ByteString] -> Inst
+parseLine (reg:op:amt:_:cReg:cComp:cInt:[]) =
+  Inst reg (readOp op) (readInt amt) (Cond cReg (readComp cComp) (readInt cInt))
+  where
+    readOp = \case
+      "inc" -> (+)
+      "dec" -> (-)
+    readComp = \case
+      "<" -> (<)
+      "<=" -> (<=)
+      "==" -> (==)
+      "!=" -> (/=)
+      ">" -> (>)
+      ">=" -> (>=)
+    readInt = maybe (error "readInt") fst . B8.readInt
 
--- | Cpu
-type Cpu = M.Map Reg Amt
-
--- | Register
-type Reg = String
-
--- | Amount/Register Value
-type Amt = Int
-
--- | Instruction
-data Inst = Inst Reg Op Amt Cond
-
--- | Condition
-data Cond = Cond Reg Comp Amt
-
--- | Operation
-type Op = (Int -> Int -> Int)
-
-readOp :: String -> Op
-readOp = \case
-  "inc" -> (+)
-  "dec" -> (-)
-  _    -> error "bad parse"
-
--- | Comparison
-type Comp = (Int -> Int -> Bool)
-
-readComp :: String -> Comp
-readComp = \case
-  "<"  -> (<)
-  "<=" -> (<=)
-  "==" -> (==)
-  "!=" -> (/=)
-  ">"  -> (>)
-  ">=" -> (>=)
-  _    -> error "bad parse"
-
--- | find the largest amount in any register
-largestAmt :: Cpu -> Amt
-largestAmt = M.foldl' max 0
-
--- | execute an instruction on the "cpu"
-runInst :: Cpu -> Inst -> Cpu
-runInst !cpu (Inst reg op amt (Cond cReg cComp cAmt)) =
-  if (get cReg `cComp` cAmt)
-    then M.insert reg (get reg `op` amt) cpu
-    else cpu
+runInst :: (Int, M.Map Reg Int) -> Inst -> (Int, M.Map Reg Int)
+runInst (high, cpu) (Inst reg op amt (Cond cReg cComp cInt)) =
+  if (get cReg `cComp` cInt)
+    then let amt' = get reg `op` amt
+         in (max amt' high, M.insert reg amt' cpu)
+    else (high, cpu)
   where
     get r = M.findWithDefault 0 r cpu
 
--- * Part One
-
+-- | Part one
 p1 :: B8.ByteString -> Int
-p1 = largestAmt . foldl' runInst M.empty . readLines
+p1 = maximum . snd . foldl' runInst (0, M.empty) . readLines
 
--- * Part Two
-
+-- | Part two
 p2 :: B8.ByteString -> Int
-p2 = maximum . tail . fmap largestAmt . scanl runInst M.empty . readLines
+p2 = fst . foldl' runInst (0, M.empty) . readLines
