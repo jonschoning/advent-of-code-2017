6ee839387bf64583135982961ae7f9a918f6c723
Jon Schoning <jonschoning@gmail.com>
2017-12-10 02:57:18 -0600

    impl day10/part2

--- haskell/src/Day10.hs
+++ haskell/src/Day10.hs
@@ -1,44 +1,62 @@
-{-# LANGUAGE BangPatterns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# OPTIONS_GHC -fno-warn-unused-do-bind #-}
-
 module Day10 where
 
-import Data.Maybe
+import Data.Char (ord)
+import Numeric (showHex)
+import Data.Bits (xor)
+import Data.Maybe (catMaybes)
 import Data.List
+import Data.List.Split (chunksOf)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Vector.Unboxed as V
 import qualified Data.Vector.Unboxed.Mutable as M
 
-import Debug.Trace
-
-init_s:: V.Vector Int
-init_s = V.fromList [0..255]
-
-size_s :: Int
-size_s = V.length init_s
-
 -- | Part one
 p1 :: B8.ByteString -> Int
-p1 = finish . foldl' go (0, 0, init_s) . readInts
+p1 = check . foldl' khRound init_khr . readInts
+ where
+  check (KHR _ _ s) = (s V.! 0) * (s V.! 1)
+  readInts =
+    fmap fst . catMaybes . fmap B8.readInt . B8.split ',' . head . B8.lines
+
+-- | Part two
+p2 :: B8.ByteString -> String
+p2 = knot_hash . head . B8.lines
+
+-- | knot_hash
+knot_hash :: B8.ByteString -> String
+knot_hash input = concatMap toHex dense_hash
+ where
+  readCodes   = B8.foldr (\b a -> ord b : a) [] 
+  length_seq  = readCodes input ++ append_l
+  expand      = concat . replicate 64
+  sparse_hash = getHash $ foldl' khRound init_khr $ expand length_seq
+  dense_hash  = foldr1 xor <$> chunksOf 16 sparse_hash
+
+data KHRound =
+  KHR !Int -- ^ pos
+      !Int -- ^ skip
+      !(V.Vector Int) -- ^ hash
+
+khRound :: KHRound -> Int -> KHRound
+khRound (KHR pos skip s) l =
+  KHR
+    ((pos + l + skip) `rem` size_h)
+    (skip + 1)
+    (s V.// zip ixs (reverse $ (s V.!) <$> ixs))
   where
-    finish (_, _, s) = (s V.! 0) * (s V.! 1)
-    go (pos, skip, s) l = do
-      let ixs = (`rem` size_s) <$> [pos,pos+1..pos+l-1]
-          s' = s V.// zip ixs (reverse $ (s V.!) <$> ixs)
-          pos' = (pos + l + skip) `rem` size_s
-          skip' = skip + 1
-          -- s'' = traceShow (pos',skip',l,s', ixs) s'
-      (pos', skip', s')
+    ixs = (`rem` size_h) <$> [pos,pos + 1 .. pos + l - 1]
 
-p2 :: B8.ByteString -> Int
-p2 _ = 0
+init_khr :: KHRound
+init_khr = KHR 0 0 (V.fromList [0 .. 255])
 
+size_h :: Int
+size_h = 256
 
--- * Utils
+append_l :: [Int]
+append_l = [17, 31, 73, 47, 23]
 
-readInts :: B8.ByteString -> [Int]
-readInts =
-  fmap fst . catMaybes . fmap B8.readInt . B8.split ','
-  . head
-  . B8.lines
+getHash :: KHRound -> [Int]
+getHash (KHR _ _ s) = V.toList s
+
+toHex :: Int -> [Char]
+toHex d = let h = showHex d "" in if length h == 1 then '0' : h else h
--- haskell/tests/Day10Spec.hs
+++ haskell/tests/Day10Spec.hs
@@ -17,7 +17,7 @@ spec = do
       it "input/day10.txt -> 23715" $ do
         input <- B8.readFile "input/day10.txt"
         D10.p1 input `shouldBe` 23715
-    -- describe "Part2" $ do
-    --   it "input/day10.txt -> 1" $ do
-    --     input <- B8.readFile "input/day10.txt"
-    --     D10.p2 input `shouldBe` 1
+    describe "Part2" $ do
+      it "input/day10.txt -> 541dc3180fd4b72881e39cf925a50253" $ do
+        input <- B8.readFile "input/day10.txt"
+        D10.p2 input `shouldBe` "541dc3180fd4b72881e39cf925a50253"
