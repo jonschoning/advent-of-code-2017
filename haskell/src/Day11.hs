0fbff1f88551b081ddf1b868bbb728d9bdfde91d
Jon Schoning <jonschoning@gmail.com>
2017-12-10 23:42:06 -0600

    impl day11/part2

--- haskell/src/Day11.hs
+++ haskell/src/Day11.hs
@@ -7,12 +7,39 @@ import Data.Foldable
 import Data.Maybe (catMaybes)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Vector.Unboxed as V
+import qualified Data.Map.Strict as M
 
--- ["n","ne","nw","s","se","sw"]
+import Debug.Trace
+
+data Cube =
+  Cube !Int -- ^ dx
+       !Int -- ^ dy
+       !Int -- ^ dz
+  deriving (Show)
+
+cube_directions :: M.Map String Cube
+cube_directions = M.fromList
+  [ ("se", Cube 1 (-1) 0)
+  , ("ne", Cube 1 0 (-1))
+  , ("n" , Cube 0 1 (-1))
+  , ("nw", Cube (-1) 1 0)
+  , ("sw", Cube (-1) 0 1)
+  , ("s" , Cube 0 (-1) 1)
+  ]
+
+cube_distance (Cube ax ay az) (Cube bx by bz) =
+    maximum [abs(ax - bx), abs(ay - by), abs(az - bz)]
+
+origin :: Cube
+origin = Cube 0 0 0
 
 -- | Part one
 p1 :: B8.ByteString -> Int
-p1 _ = 0
+p1 = cube_distance origin . foldl' move origin . readLines
+  where
+    move (Cube x y z) s =
+      let Just (Cube dx dy dz) = M.lookup s cube_directions
+      in Cube (x+dx) (y+dy) (z+dz)
 
 -- | Part two
 p2 :: B8.ByteString -> Int
--- haskell/tests/Day11Spec.hs
+++ haskell/tests/Day11Spec.hs
@@ -18,9 +18,9 @@ spec = do
       it "ne,ne,sw,sw" $ D11.p1 "ne,ne,sw,sw\n" `shouldBe` 0
       it "ne,ne,s,s" $ D11.p1 "ne,ne,s,s\n" `shouldBe` 2
       it "se,sw,se,sw,sw" $ D11.p1 "se,sw,se,sw,sw\n" `shouldBe` 3
-      -- it "input/day11.txt -> 1" $ do
-      --   input <- B8.readFile "input/day11.txt"
-      --   D11.p1 input `shouldBe` 1
+      it "input/day11.txt -> 1" $ do
+        input <- B8.readFile "input/day11.txt"
+        D11.p1 input `shouldBe` 877
     -- describe "Part2" $ do
     --   it "input/day11.txt -> 1" $ do
     --     input <- B8.readFile "input/day11.txt"
