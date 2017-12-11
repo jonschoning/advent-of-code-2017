260520733ebb24a7487812b7b3b4cc5a73e04914
Jon Schoning <jonschoning@gmail.com>
2017-12-10 22:46:16 -0600

    add bench/criterion

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 import System.Environment (getArgs)
+import Criterion (nf, benchmark)
+import Control.DeepSeq (NFData, rnf)
 import qualified Data.ByteString.Char8 as B8
-
 import qualified Day1 as D1
 import qualified Day2 as D2
 import qualified Day3 as D3
@@ -14,30 +17,39 @@ import qualified Day8 as D8
 import qualified Day9 as D9
 import qualified Day10 as D10
 
+data AnyShow = forall a. (NFData a, Show a) => S a 
+instance Show AnyShow where showsPrec p (S a) = showsPrec p a
+instance NFData AnyShow where rnf (S a) = rnf a
+
 main :: IO ()
 main = do
-  (day:part:rest) <- getArgs
-  let (inputfile:_) = rest
+  (cmd:day:part:inputfile:_) <- getArgs
+  let dp = (day, part)
   input <- B8.readFile inputfile
-  putStrLn $ case (day, part) of
-    ("1", "1") -> show $ D1.p1zip input
-    ("1", "2") -> show $ D1.p2zip input
-    ("2", "1") -> show $ D2.p1 input
-    ("2", "2") -> show $ D2.p2 input
-    ("3", "1") -> show $ D3.p1 input
-    ("3", "2") -> show $ D3.p2 input
-    ("4", "1") -> show $ D4.p1 input
-    ("4", "2") -> show $ D4.p2 input
-    ("5", "1") -> show $ D5.p1 input
-    ("5", "2") -> show $ D5.p2 input
-    ("6", "1") -> show $ D6.p1 input
-    ("6", "2") -> show $ D6.p2 input
-    ("7", "1") ->        D7.p1 input
-    ("7", "2") -> show $ D7.p2 input
-    ("8", "1") -> show $ D8.p1 input
-    ("8", "2") -> show $ D8.p2 input
-    ("9", "1") -> show $ D9.p1 input
-    ("9", "2") -> show $ D9.p2 input
-    ("10", "1") -> show $ D10.p1 input
-    ("10", "2") -> show $ D10.p2 input
-    _          -> "TODO"
+  case cmd of
+    "b" -> benchmark (nf (go dp) input)
+    "p" -> print ((go dp) input)
+    _   -> error "invalid cmd"
+ where
+  go = \case
+    ("1" , "1") -> S . D1.p1zip
+    ("1" , "2") -> S . D1.p2zip
+    ("2" , "1") -> S . D2.p1
+    ("2" , "2") -> S . D2.p2
+    ("3" , "1") -> S . D3.p1
+    ("3" , "2") -> S . D3.p2
+    ("4" , "1") -> S . D4.p1
+    ("4" , "2") -> S . D4.p2
+    ("5" , "1") -> S . D5.p1
+    ("5" , "2") -> S . D5.p2
+    ("6" , "1") -> S . D6.p1
+    ("6" , "2") -> S . D6.p2
+    ("7" , "1") -> S . D7.p1
+    ("7" , "2") -> S . D7.p2
+    ("8" , "1") -> S . D8.p1
+    ("8" , "2") -> S . D8.p2
+    ("9" , "1") -> S . D9.p1
+    ("9" , "2") -> S . D9.p2
+    ("10", "1") -> S . D10.p1
+    ("10", "2") -> S . D10.p2
+    _           -> error "invalid arguments"
