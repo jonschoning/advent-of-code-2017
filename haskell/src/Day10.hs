56f13ac6cd8a0384442cffa0add24a7f4b85428a
Jon Schoning <jonschoning@gmail.com>
2017-12-10 20:43:47 -0600

    mut

--- haskell/src/Day10.hs
+++ haskell/src/Day10.hs
@@ -1,20 +1,24 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
+
 module Day10 where
 
-import Data.Char (ord)
-import Numeric (showHex)
+import Control.Monad.ST (runST, ST)
 import Data.Bits (xor)
-import Data.Maybe (catMaybes)
+import Data.Char (ord)
 import Data.List
+import Data.Foldable
 import Data.List.Split (chunksOf)
+import Data.Maybe (catMaybes)
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Vector.Unboxed as V
-import qualified Data.Vector.Unboxed.Mutable as M
+import qualified Data.Vector.Unboxed.Mutable as MV
+import Text.Printf (printf)
 
 -- | Part one
 p1 :: B8.ByteString -> Int
-p1 = check . foldl' khRound init_khr . readInts
+p1 = check . sparse_hash . readInts
  where
-  check (KHR _ _ s) = (s V.! 0) * (s V.! 1)
+  check (a:b:_) = a * b
   readInts =
     fmap fst . catMaybes . fmap B8.readInt . B8.split ',' . head . B8.lines
 
@@ -22,41 +26,40 @@ p1 = check . foldl' khRound init_khr . readInts
 p2 :: B8.ByteString -> String
 p2 = knot_hash . head . B8.lines
 
--- | knot_hash
-knot_hash :: B8.ByteString -> String
-knot_hash input = concatMap toHex dense_hash
- where
-  readCodes   = B8.foldr (\b a -> ord b : a) [] 
-  length_seq  = readCodes input ++ append_l
-  expand      = concat . replicate 64
-  sparse_hash = getHash $ foldl' khRound init_khr $ expand length_seq
-  dense_hash  = foldr1 xor <$> chunksOf 16 sparse_hash
+-- * knot_hash
 
-data KHRound =
+data KHR s =
   KHR !Int -- ^ pos
       !Int -- ^ skip
-      !(V.Vector Int) -- ^ hash
+      !(MV.STVector s Int) -- ^ hash
 
-khRound :: KHRound -> Int -> KHRound
-khRound (KHR pos skip s) l =
-  KHR
-    ((pos + l + skip) `rem` size_h)
-    (skip + 1)
-    (s V.// zip ixs (reverse $ (s V.!) <$> ixs))
+knot_hash :: B8.ByteString -> String
+knot_hash input =
+  let readLengths = B8.foldr (\b a -> ord b : a) []
+      salt = [17, 31, 73, 47, 23]
+      length_seq = readLengths input ++ salt
+      expand = concat . replicate 64
+      dense_hash = foldl1' xor <$> chunksOf 16 (sparse_hash (expand length_seq))
+  in concatMap (printf "%02x") dense_hash
+
+sparse_hash :: [Int] -> [Int]
+sparse_hash length_seq = V.toList $ runST $ do
+  khr           <- init_khr
+  KHR _ _ hash' <- foldlM kh_round khr length_seq
+  V.unsafeFreeze hash'
+
+kh_round :: KHR s -> Int -> ST s (KHR s)
+kh_round (KHR pos skip hash) n = do
+  let ixs = (`rem` size_h) <$> [pos .. pos + n - 1]
+  xs <- traverse hRead ixs
+  traverse_ hWrite $ zip ixs (reverse xs)
+  pure $ KHR (pos + skip + n `rem` size_h) (skip + 1) hash
   where
-    ixs = (`rem` size_h) <$> [pos,pos + 1 .. pos + l - 1]
+    hRead = MV.unsafeRead hash
+    hWrite = uncurry (MV.unsafeWrite hash)
 
-init_khr :: KHRound
-init_khr = KHR 0 0 (V.fromList [0 .. 255])
+init_khr :: ST s (KHR s)
+init_khr = fmap (KHR 0 0) (V.thaw (V.fromList [0 .. size_h - 1]))
 
 size_h :: Int
 size_h = 256
-
-append_l :: [Int]
-append_l = [17, 31, 73, 47, 23]
-
-getHash :: KHRound -> [Int]
-getHash (KHR _ _ s) = V.toList s
-
-toHex :: Int -> [Char]
-toHex d = let h = showHex d "" in if length h == 1 then '0' : h else h
