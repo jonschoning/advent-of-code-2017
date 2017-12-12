d92e6c7bd66d56afe3faf1a111084c76afcf4556
Jon Schoning <jonschoning@gmail.com>
2017-12-11 23:59:53 -0600

    impl day12/part1

--- haskell/src/Day12.hs
+++ haskell/src/Day12.hs
@@ -1,11 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Day12 where
 
-import Data.List
 import Data.Char
-import Data.Monoid
-import Data.Maybe
 import qualified Data.ByteString.Char8 as B8
 import qualified Data.Map.Strict as M
 import Data.Set (Set)
@@ -13,11 +11,31 @@ import qualified Data.Set as Set
 
 -- | Part one
 p1 :: B8.ByteString -> Int
-p1 _ = 0
+p1 input = finish $ go (readLines input) 0
+  where
+    finish grp = Set.size grp
+
+go :: M.Map Int (Set Int) -> Int -> Set Int
+go m i = go' (Set.singleton 0) (Set.empty) m i
+
+go' :: Set Int -> Set Int -> M.Map Int (Set Int) -> Int -> Set Int
+go' !grp !seen !m !i = do
+    if (Set.member i seen)
+    then grp
+    else case M.lookup i m of
+            Just x -> do
+                let xs = Set.toList x
+                    seen' = Set.insert i seen
+                    grps = fmap (\x' -> go' grp seen' m x') xs
+                Set.unions (x : grp : grps)
+            Nothing -> grp
 
 -- | Part two
 p2 :: B8.ByteString -> Int
-p2 _ = 0
+p2 input =
+  let m = readLines input
+      grps = Set.map (go m) (Set.unions (M.elems m))
+  in Set.size grps
 
 -- * Utils
 
--- haskell/tests/Day12Spec.hs
+++ haskell/tests/Day12Spec.hs
@@ -5,7 +5,7 @@ module Day12Spec where
 import Test.Hspec
 import qualified Data.ByteString.Char8 as B8
 
-import qualified Day11 as D12
+import qualified Day12 as D12
 
 main :: IO ()
 main = hspec spec
@@ -14,10 +14,10 @@ spec :: Spec
 spec = do
 
     describe "Part1" $ do
-      it "input/day12.txt -> 877" $ do
+      it "input/day12.txt -> 169" $ do
         input <- B8.readFile "input/day12.txt"
-        D12.p1 input `shouldBe` 877
+        D12.p1 input `shouldBe` 169
     describe "Part2" $ do
-      it "input/day12.txt -> 1622" $ do
+      it "input/day12.txt -> 179" $ do
         input <- B8.readFile "input/day12.txt"
-        D12.p2 input `shouldBe` 1622
+        D12.p2 input `shouldBe` 179
